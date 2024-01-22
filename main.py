9cb2c8ec1bda773a387e88d9a4c5fb6cfcfb28ff
Sushil <sushilwaghmare2048@gmail.com>
2024-01-22 13:53:55 +0530

    remove static - unwanted code

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
