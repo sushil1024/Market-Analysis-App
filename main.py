f2c5a5916679b02c6a352aefb608ffd25b7d7c53
Sushil <sushilwaghmare2048@gmail.com>
2024-01-22 14:13:02 +0530

    Docstring

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+'''
+Project: Market Analysis App
+Author: Sushil Waghmare
+Date started: 22-01-2024
+Last updated: 22-01-2024
+Organization: Finzome Technologies
+Github Link: https://github.com/sushil1024/Market-Analysis-App
+App Home Page Endpoint: https://market-analysis-app.onrender.com
+Request Endpoint Link: https://market-analysis-app.onrender.com/output
+Method: POST
+Request Body (form-data):
+    Key: File
+    Value: ---Upload CSV File---
+'''
+
 import pandas as pd
 import numpy as np
 import json
